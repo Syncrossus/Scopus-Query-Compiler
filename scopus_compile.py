d18dbcaf510ba87eddb0389573e2bcf79b89bbca
Guilhem Piat <guilhempiat@yahoo.fr>
2019-08-24 01:30:10 +0200

    debugging
    print debugging messages
    selection is now updating properly
    fixed function calls
    fixed badly indented instruction causing it to repeat
    separated comment deselection into its own function
    fixed substring selection for resolving query references
    fixed missing parentheses in compiled query

--- scopus_compile.py
+++ scopus_compile.py
@@ -2,59 +2,62 @@ import re
 import sublime_plugin
 
 from sublime import Region
-from sublime import Selection
 
 
 class CompileScopusQueryCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
+        print("running")
         selection = self.view.sel()
-        new_selection = Selection(12)
+        new_selection = []
 
         # splitting lines over line breaks
+        print("splitting lines over line breaks")
         for region in selection:
             text = self.view.substr(region)
+            print(text)
             # adding a virtual line break before the region
             # because we take the regions between line breaks
             line_break_positions = [-1]
             for i in range(len(text)):
                 if text[i] == '\n':
-                    line_break_positions.append[i]
+                    line_break_positions.append(i)
 
                 # If region doesn't end on a line break,
                 # adding a virtual line break after the
                 # last character (same reason as before)
-                if text[-1] != '\n':
-                    line_break_positions.append(len(text) + 1)
+            if text[-1] != '\n':
+                line_break_positions.append(len(text) + 1)
 
+            print("line_break_positions: ", [self.view.rowcol(i) for i in line_break_positions])
             # isolating all the regions between line breaks
             for i in range(len(line_break_positions) - 1):
-                new_selection.add(Region(
+                print(self.view.rowcol(region.a + line_break_positions[i] + 1),
+                      self.view.rowcol(region.a + line_break_positions[i + 1]))
+                new_selection.append(Region(
                     region.a + line_break_positions[i] + 1,
                     region.a + line_break_positions[i + 1]))
 
         # overwriting old selection
-        selection = new_selection
-        new_selection = Selection(13)
+        selection.clear()
+        selection.add_all(new_selection)
+        new_selection = []
 
         # unselecting comments
+        print("unselecting comments")
         for region in selection:
-            text = self.view.substr(region)
-            comment_start = text.find('%')
-            if comment_start != -1:
-                new_selection.add(Region(
-                    region.a,
-                    region.a + comment_start))
-            else:
-                new_selection.add(region)
+            new_selection.append(self.remove_comments(region))
 
         # overwriting old selection
-        selection = new_selection
+        selection.clear()
+        selection.add_all(new_selection)
 
         # starting the compilation process
+        print("compiling")
         line_ref_pattern = re.compile(r"#[0-9]+")
         for region in selection:
             text = self.view.substr(region)
+            print("before: ", text)
             current_line_number = self.view.rowcol(region.a)[0]
             # getting each occurence of the form "#12"
             # which references a line in the file
@@ -62,7 +65,7 @@ class CompileScopusQueryCommand(sublime_plugin.TextCommand):
             while reference is not None:
                 # getting the referenced line
                 target_line_number = int(
-                    text[reference.start():reference.end()]) - 1
+                    text[reference.start() + 1:reference.end()]) - 1
 
                 if target_line_number == current_line_number:
                     raise RecursionError(
@@ -74,12 +77,21 @@ class CompileScopusQueryCommand(sublime_plugin.TextCommand):
                     target_line_number + 1, 0) - 1
 
                 # replacing the reference with its target
-                text = (text[:reference.start()] +
+                text = (text[:reference.start()] + '(' +
                         self.view.substr(Region(target_line_start,
                                                 target_line_end)) +
-                        text[reference.end():])
+                        ')' + text[reference.end():])
                 reference = line_ref_pattern.search(text)
-            print(text)
+            print("after: ", text)
             self.view.replace(edit, region, text)
 
         # caret_pos = selection[-1].b
+
+    def remove_comments(self, region):
+        text = self.view.substr(region)
+        print(text)
+        comment_start = text.find('%')
+        if comment_start != -1:
+            return Region(region.a, region.a + comment_start)
+        else:
+            return region
