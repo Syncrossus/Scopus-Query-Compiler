da105bcf3b2b5e6ebb09250bfa7b7b4cba500fcc
Guilhem Piat <guilhempiat@yahoo.fr>
2019-08-26 00:38:36 +0200

    readability and stability improvements
    commented out debugging printing
    separated line break handling into its own function
    handled case where no text is selected

--- scopus_compile.py
+++ scopus_compile.py
@@ -7,36 +7,18 @@ from sublime import Region
 class CompileScopusQueryCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
-        print("running")
+        # compiling ahead of time regex for recognizing query references
+        self.line_ref_pattern = re.compile(r"#[0-9]+")
+
+        # print("running")
         selection = self.view.sel()
         new_selection = []
 
         # splitting lines over line breaks
-        print("splitting lines over line breaks")
+        # print("splitting lines over line breaks")
         for region in selection:
-            text = self.view.substr(region)
-            print(text)
-            # adding a virtual line break before the region
-            # because we take the regions between line breaks
-            line_break_positions = [-1]
-            for i in range(len(text)):
-                if text[i] == '\n':
-                    line_break_positions.append(i)
-
-                # If region doesn't end on a line break,
-                # adding a virtual line break after the
-                # last character (same reason as before)
-            if text[-1] != '\n':
-                line_break_positions.append(len(text) + 1)
-
-            print("line_break_positions: ", [self.view.rowcol(i) for i in line_break_positions])
-            # isolating all the regions between line breaks
-            for i in range(len(line_break_positions) - 1):
-                print(self.view.rowcol(region.begin() + line_break_positions[i] + 1),
-                      self.view.rowcol(region.begin() + line_break_positions[i + 1]))
-                new_selection.append(Region(
-                    region.begin() + line_break_positions[i] + 1,
-                    region.begin() + line_break_positions[i + 1]))
+            region = self.fix_empty_region(region)
+            new_selection.append(self.split_line_breaks(region))
 
         # overwriting old selection
         selection.clear()
@@ -44,56 +26,96 @@ class CompileScopusQueryCommand(sublime_plugin.TextCommand):
         new_selection = []
 
         # unselecting comments
-        print("unselecting comments")
+        # print("unselecting comments")
         for region in selection:
-            new_selection.append(self.remove_comments(region))
+            region = self.fix_empty_region(region)
+            new_selection.extend(self.remove_comments(region))
 
         # overwriting old selection
         selection.clear()
         selection.add_all(new_selection)
 
         # starting the compilation process
-        print("compiling")
-        line_ref_pattern = re.compile(r"#[0-9]+")
+        # print("compiling")
         for region in selection:
-            text = self.view.substr(region)
-            print("before: ", text)
-            current_line_number = self.view.rowcol(region.begin())[0]
-            # getting each occurence of the form "#12"
-            # which references a line in the file
-            reference = line_ref_pattern.search(text)
-            while reference is not None:
-                # getting the referenced line
-                target_line_number = int(
-                    text[reference.start() + 1:reference.end()]) - 1
+            region = self.fix_empty_region(region)
+            self.view.replace(edit, region, self.compile_query(region))
 
-                if target_line_number == current_line_number:
-                    raise RecursionError(
-                        "Self-referencing query would have caused infinite recursion.")
+    def compile_query(self, region):
+        text = self.view.substr(region)
+        # print("before: ", text)
+        current_line_number = self.view.rowcol(region.begin())[0]
+        # getting each occurence of the form "#12"
+        # which references a line in the file
+        reference = self.line_ref_pattern.search(text)
+        while reference is not None:
+            # getting the referenced line
+            target_line_number = int(
+                text[reference.start() + 1:reference.end()]) - 1
 
-                target_line_start = self.view.text_point(target_line_number,
-                                                         0)
-                target_line_end = self.view.text_point(
-                    target_line_number + 1, 0) - 1
+            if target_line_number == current_line_number:
+                raise RecursionError(
+                    "Self-referencing query would have caused infinite recursion.")
 
-                # replacing the reference with its target
-                text = (text[:reference.start()] + '(' +
-                        self.view.substr(
-                            self.remove_comments(
-                                Region(target_line_start,
-                                       target_line_end))) +
-                        ')' + text[reference.end():])
-                reference = line_ref_pattern.search(text)
-            print("after: ", text)
-            self.view.replace(edit, region, text)
+            target_line_start = self.view.text_point(target_line_number, 0)
+            target_line_end = self.view.text_point(
+                target_line_number + 1, 0) - 1
 
-        # caret_pos = selection[-1].b
+            # replacing the reference with its target
+            text = (text[:reference.start()] + '(' +
+                    self.view.substr(
+                        self.remove_comments(
+                            Region(target_line_start,
+                                   target_line_end))) +
+                    ')' + text[reference.end():])
+            reference = self.line_ref_pattern.search(text)
+        # print("after: ", text)
+        return text
+
+    def fix_empty_region(self, region):
+        if region.a != region.b:
+            return region
+        else:
+            caret_pos = self.view.rowcol(region.a)
+            print("caret_pos: ", caret_pos)
+            print("region begin: ", self.view.text_point(caret_pos[0], 0))
+            print("region end: ", self.view.text_point(caret_pos[0] + 1, 0) - 1)
+            return Region(
+                self.view.text_point(caret_pos[0], 0),
+                self.view.text_point(caret_pos[0] + 1, 0) - 1)
 
     def remove_comments(self, region):
         text = self.view.substr(region)
-        print(text)
+        # print(text)
         comment_start = text.find('%')
         if comment_start != -1:
             return Region(region.begin(), region.begin() + comment_start)
         else:
             return region
+
+    def split_line_breaks(self, region):
+        text = self.view.substr(region)
+        # print(text)
+        # adding a virtual line break before the region
+        # because we take the regions between line breaks
+        line_break_positions = [-1]
+        for i in range(len(text)):
+            if text[i] == '\n':
+                line_break_positions.append(i)
+
+            # If region doesn't end on a line break,
+            # adding a virtual line break after the
+            # last character (same reason as before)
+        if text[-1] != '\n':
+            line_break_positions.append(len(text) + 1)
+
+        # print("line_break_positions: ", [self.view.rowcol(i) for i in line_break_positions])
+
+        new_regions = []
+        # isolating all the regions between line breaks
+        for i in range(len(line_break_positions) - 1):
+            # print(self.view.rowcol(region.begin() + line_break_positions[i] + 1),
+                  self.view.rowcol(region.begin() + line_break_positions[i + 1]))
+            new_regions.append(Region(
+                region.begin() + line_break_positions[i] + 1,
+                region.begin() + line_break_positions[i + 1]))
